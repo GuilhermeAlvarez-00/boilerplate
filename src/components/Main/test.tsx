e02b68ce67d8375780a31cc4014027fc0bd152e9
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-07-21 12:01:40 -0300

    feat: creating first styles for Main component

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -10,6 +10,12 @@ describe('<Main />', () => {
 			screen.getByRole('heading', { name: /react avançado/i })
 		).toBeInTheDocument()
 
-		expect(container).toMatchSnapshot()
+		expect(container.firstChild).toMatchSnapshot()
+	})
+
+	it('should render the color correctly', () => {
+		const { container } = render(<Main />)
+
+		expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
 	})
 })
