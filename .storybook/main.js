9e504dd9d474486c5e7cd2ce72580854812e9520
GuilhermeAlvarez-00 <gui06barboza@gmail.com>
2022-07-23 13:43:52 -0300

    fix: static files for build storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,15 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/components/**/*stories.mdx",
     "../src/components/**/*stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
   ],
-  "framework": "@storybook/react",
-  "core": {
+  framework: "@storybook/react",
+  core: {
     "builder": "@storybook/builder-webpack5"
   }
 }
